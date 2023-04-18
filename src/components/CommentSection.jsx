7a17e103c5c07b136d5e4b790e8a74e519024bec
zaneris123 <zaneris123@gmail.com>
2023-04-18 16:17:05 +0100

    comment section complete

--- src/components/CommentSection.jsx
+++ src/components/CommentSection.jsx
@@ -1,12 +1,22 @@
 import { useEffect } from "react"
 import { useState } from "react"
 import { getReviewComments } from "./api"
+import { useParams } from "react-router-dom"
+import CommentCard from "./CommentCard"
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material"
 
 
 
 function CommentSection (){
+    const {reviewID} = useParams()
     const [reviewComments, setReviewComments] = useState([])
     const [isLoading, setIsLoading] = useState(true)
+    const [showComments, setShowcomments] = useState(false)
+
+    const commentHandler = (event) => {
+        event.preventDefault()
+        setShowcomments(!showComments)
+    }
 
     useEffect(()=>{
         setIsLoading(true)
@@ -15,11 +25,29 @@ function CommentSection (){
             setReviewComments(commentData)
             setIsLoading(false)
         })
-    },[])
+    },[reviewID])
 
     return isLoading ? (<p>Loading...</p>):(
         <section>
-            <h4>Comments - {reviewComments.length()}</h4>
+            <button onClick={commentHandler}>Comments - {reviewComments.length}</button>
+            {!showComments ? null :             
+            <TableContainer>
+                <Table>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Comment</TableCell>
+                            <TableCell>Posted by</TableCell>
+                            <TableCell><span>📅</span></TableCell>
+                            <TableCell><span>🗳️</span></TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {reviewComments.map((comment)=><CommentCard key={comment.comment_id} comment={comment}/>)}
+                    </TableBody>
+                </Table>
+            </TableContainer>}
+            <p></p>
         </section>
     )
 }
+export default CommentSection
