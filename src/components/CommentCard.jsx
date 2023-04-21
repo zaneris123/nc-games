6500dcb1d0eeb17fa44e638699ebb1832c302e76
zaneris123 <zaneris123@gmail.com>
2023-04-21 16:01:02 +0100

    fixing merger

--- src/components/CommentCard.jsx
+++ src/components/CommentCard.jsx
@@ -1,6 +1,9 @@
 import { TableCell, TableRow } from "@mui/material"
+import { UserContext } from "../contexts/user"
+import { useContext } from "react"
+import { deleteComment } from "./api"
 
-function CommentCard ({comment}){
+function CommentCard ({comment, setReviewComments}){
     const commentDate = new Date(comment.created_at.replace(' ', 'T'))
     const { userObj } = useContext(UserContext)
     const DeleteHandler = (event) => {
@@ -15,12 +18,14 @@ function CommentCard ({comment}){
             })
         }
     }
+
     return(
         <TableRow>
             <TableCell>{comment.body}</TableCell>
             <TableCell>{comment.author}</TableCell>
             <TableCell>{`${commentDate.getDate()}/${commentDate.getMonth()+1}/${commentDate.getFullYear()}`}</TableCell>
             <TableCell>{comment.votes}</TableCell>
+            <TableCell><button onClick={DeleteHandler} disabled={!(comment.author===userObj.username)}><span>🗑️</span></button></TableCell>
         </TableRow>
     )
 }
