cb8825a302f53e55f898c4a0dce45e2c5d14be8f
zaneris123 <zaneris123@gmail.com>
2023-04-21 16:04:37 +0100

    added api function for delete

--- src/components/api.js
+++ src/components/api.js
@@ -47,3 +47,6 @@ export const getCategories = () => {
         return data.categories
     })
 }
+export const deleteComment = (commentId) => {
+    return gamesApi.delete(`/comments/${commentId}`)
+}
